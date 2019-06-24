836998672a753041677e590902736e129921136a
Junnnior <juniorfoxld@hotmail.com>
2019-06-24 19:13:28 -0300

    Criando tabs Scroll Horizontal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { Container, Top, Logo, Title } from './styles';
-import logo from '~/assets/Nubank_Logo.png';
+import {
+  Container, Top, Logo, Title,
+} from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '~/assets/Nubank_Logo.png';
 
 export default function Header() {
   return (
--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container, TabsContainer, TabItem, TabText,
+} from './styles';
 
 export default function Tabs() {
   return (
-    <Container />
+    <Container>
+      <TabsContainer>
+        {/* Tab indicar Amigos */}
+        <TabItem>
+          <Icon name="person-add" size={24} color="#FFF" />
+          <TabText>Indicar amigos</TabText>
+        </TabItem>
+
+        {/* Tab Cobrar */}
+        <TabItem>
+          <Icon name="chat-bubble-outline" size={24} color="#FFF" />
+          <TabText>Cobrar</TabText>
+        </TabItem>
+
+        {/* Tab Depositar */}
+        <TabItem>
+          <Icon name="arrow-downward" size={24} color="#FFF" />
+          <TabText>Depositar</TabText>
+        </TabItem>
+
+        {/* Tab transferir */}
+        <TabItem>
+          <Icon name="arrow-upward" size={24} color="#FFF" />
+          <TabText>Transferir</TabText>
+        </TabItem>
+
+        {/* Tab Bloquear Cartão */}
+        <TabItem>
+          <Icon name="lock" size={24} color="#FFF" />
+          <TabText>Bloquear Cartão</TabText>
+        </TabItem>
+      </TabsContainer>
+    </Container>
   );
 }
